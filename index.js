80c6c50703173fd280643791157340aa99768b12
agj <ale@agj.cl>
2018-03-02 19:11:32 -0300

    Basic functionality done.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const C = require('chalk');
 const tmp = require('tmp-promise');
 const open = require('opn');
 const watch = require('node-watch');
+const flyd = require('flyd');
 require('dot-into').install();
 
 
@@ -19,11 +20,23 @@ const log = (...msg) => {
 };
 const prepend = pre => text => pre + text;
 const onUpdate = R.when(R.equals('update'));
-const noPath = R.pipe(path.parse, R.prop('name'));
-const message = msg => console.log(C.green(msg.split('\n').map(prepend('   ')).join('\n')));
+const justName = R.pipe(path.parse, R.prop('base'));
+const justDir = R.pipe(path.parse, R.prop('dir'));
+const message = (msg, color = 'white') => console.log(C[color](msg.split('\n').map(prepend('   ')).join('\n')));
 const error = msg => {
 	console.error(C.red('   ' + msg));
 	throw msg;
+};
+const streamMap = flyd.map;
+const streamFilter = require('flyd/module/filter');
+const streamTwo = stream => {
+	const r = flyd.stream();
+	let prev = stream();
+	stream.into(flyd.on(v => {
+		r([prev, v]);
+		prev = v;
+	}));
+	return r;
 };
 
 
@@ -35,43 +48,65 @@ const error = msg => {
 	.usage("<file ...>")
 	.parse(process.argv);
 
-	const filenames = program.args;
+	const startFilenames = program.args;
 
-	if (filenames.length === 0) {
+	if (startFilenames.length === 0) {
 		program.help();
 		process.exit(1);
 
 	} else {
-		const filenamesNoPath = filenames.map(noPath);
 		const tempfile = await tmp.file({ postfix: '.txt', prefix: 'rename-' });
+		const filenames = flyd.stream(startFilenames);
+		const filenamesNoPath =
+			filenames
+			.into(streamMap(R.map(justName)));
 
 		message(`Opening filename list at: ${ tempfile.path }`);
 
-		const tempContents = () =>
-			fs.readFileSync(tempfile.path, 'utf-8')
-			.split('\n');
+		const tempContents = flyd.stream([]);
 
-		fs.writeFile(tempfile.path, filenamesNoPath.join('\n'), 'utf-8');
+		fs.writeFile(tempfile.path, filenamesNoPath().join('\n'), 'utf-8');
 		open(tempfile.path);
 
-		const watcher = watch(tempfile.path);
-		watcher.on('change', onUpdate(() => {
-			const newFilenames = tempContents();
+		const tempWatcher = watch(tempfile.path);
+		tempWatcher.on('change', onUpdate(() =>
+			tempContents(
+				fs.readFileSync(tempfile.path, 'utf-8')
+				.split('\n'))));
+
+		tempContents
+		.into(flyd.on((newNames) => {
 			try {
-				if (newFilenames.length !== filenames.length)
+				if (newNames.length !== filenames().length)
 					error("New filename list has more or fewer lines than the original!");
-				if (R.uniq(newFilenames).length !== newFilenames.length)
+				if (R.uniq(newNames).length !== newNames.length)
 					error("Some filenames are identical!");
-				if (newFilenames.filter(R.isEmpty).length > 0)
+				if (newNames.filter(R.isEmpty).length > 0)
 					error("Some lines are empty!");
+				if (newNames.filter(n => path.normalize(n).replace(path.sep, '') !== n).length > 0)
+					error("Illegal filename used!");
 
-				const changed =
-					R.zip(filenamesNoPath, newFilenames)
-					.filter(([a, b]) => a !== b);
-
-				changed.forEach(([ol, nw]) => message(`${ ol }\n-> ${ nw }`));
-
+				filenames(
+					R.zip(filenames().map(justDir), newNames)
+					.map(R.apply(path.join)));
 			} catch (e) { }
+		}));
+
+		filenames
+		.into(streamTwo)
+		.into(streamFilter(([a, b]) => a !== b))
+		.into(flyd.on(([oldNames, newNames]) => {
+			const changes =
+				R.zip(oldNames, newNames)
+				.filter(([a, b]) => a !== b);
+			if (changes.length > 0) {
+				message("Filename changes:");
+				changes.forEach(([oldName, newName]) => {
+					message(` • ${ justName(oldName) }`, 'gray');
+					message(` > ${ justName(newName) }`, 'green');
+					fs.renameSync(oldName, newName);
+				});
+			}
 		}));
 	}
 
